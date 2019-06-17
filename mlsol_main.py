6c83c73d3dea6aa7bc86f0fc1811a93d128cb2b1
diliadis <JimmyHliad@gmail.com>
2019-06-17 18:37:21 +0300

    fixed various small bugs

--- mlsol_main.py
+++ mlsol_main.py
@@ -1,7 +1,7 @@
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
 import random
-
+from sklearn.datasets import make_multilabel_classification
 
 def get_Cij(sample_label, neighbours_label_values):
     num_common_labels = np.count_nonzero(neighbours_label_values == sample_label)
@@ -11,6 +11,7 @@ def get_Cij(sample_label, neighbours_label_values):
 
 # for each label lj we compute the proportion of neighbours having opposite class with respect to the class of the
 # instance and store the result in the matrix C
+# a zero value indicates a safe neighborhood and a value of one indicates a hostile neighborhood
 def get_C(y, indices):
     C = np.zeros(y.shape)
     for i in range(y.shape[0]):
@@ -20,18 +21,18 @@ def get_C(y, indices):
 
 
 # aggregate the values in C per training example in order to arrive at a singe sampling weight wi
-def get_weight_per_example(y, C):
+def get_weight_per_example(y, C, min_class_per_label):
     w = np.zeros(y.shape[0])
     sum_of_non_out_minority_examples_per_example = np.zeros(y.shape[1])
     for j in range(y.shape[1]):
         for i in range(y.shape[0]):
-            if y[i, j] == 1 and C[i, j] < 1:
+            if y[i, j] == min_class_per_label[j] and C[i, j] < 1:
                 sum_of_non_out_minority_examples_per_example[j] += C[i, j]
 
     for i in range(y.shape[0]):
         sum = 0
         for j in range(y.shape[1]):
-            if y[i, j] == 1 and C[i, j] < 1:
+            if y[i, j] == min_class_per_label[j] and C[i, j] < 1:
                 sum += C[i, j] / sum_of_non_out_minority_examples_per_example[j]
         w[i] = sum
 
@@ -44,14 +45,14 @@ def get_min_class_per_label(y):
         class_1 = np.count_nonzero(y[:, i] == 1)
         class_0 = np.count_nonzero(y[:, i] == 0)
         if class_1 >= class_0:
-            min_per_label[i] = 1
-        else:
             min_per_label[i] = 0
+        else:
+            min_per_label[i] = 1
     return min_per_label
 
 
-def get_type_matrix(y, C, k, min_class_per_label, neighbour_indices):
-    T = np.zeros(C.shape)
+def get_type_matrix(y, C, min_class_per_label, neighbour_indices):
+    T = np.empty(C.shape, dtype="U2")
     for i in range(C.shape[0]):
         for j in range(C.shape[1]):
             if y[i, j] == min_class_per_label[j]:
@@ -115,16 +116,12 @@ def generate_instance(x_seed, y_seed, T_seed, x_reference, y_reference, T_refere
 
             if T_seed[j] == 'SF':
                 theta = 0.5
-                break
             elif T_seed[j] == 'BD':
                 theta = 0.75
-                break
             elif T_seed[j] == 'RR':
                 theta = 1.00005
-                break
             elif T_seed[j] == 'OT':
                 theta = -0.00005
-                break
 
             if cd <= theta:
                 y_synthetic[j] = y_seed[j]
@@ -140,29 +137,50 @@ def mlsol_main(X, y, perc_gen_instances=0.3, k=5):
     min_class_per_label = get_min_class_per_label(y)
 
     # number of instances to generate
-    gen_num = X.shape[0] * perc_gen_instances
+    gen_num = int(X.shape[0] * perc_gen_instances)
 
     X_synthesized, y_synthesized = np.zeros((gen_num, X.shape[1])), np.zeros((gen_num, y.shape[1]))
 
     # find the kNN of each instance
     # the sklearn implementation suggests that for small number of neighbours the brute force method is comparable
     # to the more computationally efficient methods (kd_tree, ball_tree)
-    nbrs = NearestNeighbors(n_neighbors=k, algorithm='brute').fit(X)
+    nbrs = NearestNeighbors(n_neighbors=k+1, algorithm='brute', metric='euclidean').fit(X)
     distances, indices = nbrs.kneighbors(X)
 
     C = get_C(y, indices)
-    w = get_weight_per_example(y, C)
-    T = get_type_matrix(y, C, k, min_class_per_label, indices)
-
+    w = get_weight_per_example(y, C, min_class_per_label)
+    T = get_type_matrix(y, C, min_class_per_label, indices)
+    counter = 0
+    init_size = gen_num
     while gen_num > 0:
         # select a seed instance from your initial dataset based on the weights vector w
         seed_index = get_seed_instance(w)
         # randomly choose a reference instance from the k nearest neighbours of the seed instance
         reference_index = indices[seed_index][random.randint(1, k)]
         # i am adding the synthesized samples in reverse inside the dataset
-        X_synthesized[gen_num], y_synthesized[gen_num] = generate_instance(X[seed_index, :], y[seed_index, :], T[seed_index, :], X[reference_index, :], y[reference_index, :], T[reference_index, :])
+        X_synthesized[gen_num-1], y_synthesized[gen_num-1] = generate_instance(X[seed_index, :], y[seed_index, :], T[seed_index, :], X[reference_index, :], y[reference_index, :], T[reference_index, :])
+        print('sample '+str(counter)+' / '+str(init_size)+' created')
+        gen_num -= 1
+        counter += 1
 
     X_aug = np.concatenate((X, np.flip(X_synthesized, axis=0)), 0)
     y_aug = np.concatenate((y, np.flip(y_synthesized, axis=0)), 0)
 
     return X_aug, y_aug
+
+
+def main():
+    X, Y = make_multilabel_classification(n_samples=500, n_classes=25, n_labels=3,
+                                          allow_unlabeled=False,
+                                          random_state=1)
+
+    Y[136, 2] = 1
+    Y[36, 2] = 1
+    Y[236, 2] = 1
+    Y[436, 2] = 1
+
+    for i in range(Y.shape[1]):
+        if sum(Y[:, i]) == 0:
+            print(str(i))
+
+    X_aug, y_aug = mlsol_main(X, Y, perc_gen_instances=0.3, k=5)
